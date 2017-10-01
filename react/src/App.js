ddc507023d494cb1871d58a746276d998aa458e6
mwinslow331 <michael.winslow331@gmail.com>
2017-10-01 13:06:05 -0400

    these are plans for routes within application

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Link, IndexRoute } from 'react-router';
-import NavBar from './containers/NavBar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   constructor(props){
@@ -12,6 +12,9 @@ class App extends Component {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={NavBar}>
+          // <Route path='new_event' name='new_event' component={NewEvent}/>
+          // <Route path='event_profile' name='event_profile' component={EventProfile}/>
+          // <Route path='user_profile' name='user_profile' component={UserProfile}/>
         </Route>
       </Router>
 
